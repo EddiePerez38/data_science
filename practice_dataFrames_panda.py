5dfc96c3b27ff2e4210550202b6014dcb56f0a32
Eddie Perez <eddie.e.perez4@gmail.com>
2019-07-19 19:15:18 -0700

    Panda Data Frames Update

--- practice_dataFrames_panda.py
+++ practice_dataFrames_panda.py
@@ -45,9 +45,10 @@ df[['W','Z']]
 df.W
 
 
-# In[10]:
+# In[18]:
 
 
+#Data frame columns are just a set of series
 type(df["W"])
 
 
@@ -88,6 +89,70 @@ df.drop('new',axis=1,inplace=True)
 
 
 df
+
+
+# In[24]:
+
+
+# Can also frop rows using (this is doesn't permanently drop rows:
+df.drop('E',axis=0)
+
+
+# In[21]:
+
+
+df
+
+
+# In[27]:
+
+
+#Selecting Rows
+df.loc['A']
+
+
+# In[28]:
+
+
+#Or select based off of a position instead of labels
+df.iloc[2]
+
+
+# In[29]:
+
+
+#Select a subset of rows
+df.loc['B','Y']
+
+
+# In[30]:
+
+
+df.loc[['A','B'],['W','Y']]
+
+
+# In[31]:
+
+
+df
+
+
+# In[32]:
+
+
+df>0
+
+
+# In[33]:
+
+
+df[df>0]
+
+
+# In[36]:
+
+
+df[df['W']>0]
 
 
 # In[ ]:
