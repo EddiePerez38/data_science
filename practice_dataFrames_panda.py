1ff7445fcbddd1cb3a395410a55d069ea8977e9a
Eddie Perez <eddie.e.perez4@gmail.com>
2019-07-19 21:27:58 -0700

    Panda Data Frames Final Update

--- practice_dataFrames_panda.py
+++ practice_dataFrames_panda.py
@@ -155,6 +155,147 @@ df[df>0]
 df[df['W']>0]
 
 
+# In[39]:
+
+
+df[df['W']>0]['Y']
+
+
+# In[40]:
+
+
+df[df['W']>0][['Y','X']]
+
+
+# In[45]:
+
+
+df[(df['W']>0) & (df['Y'] > 1)]
+
+
+# In[46]:
+
+
+df
+
+
+# In[47]:
+
+
+#Reset to default 0,1...n index
+df.reset_index()
+
+
+# In[50]:
+
+
+newind = 'CA NY WY OR CO'.split()
+
+
+# In[52]:
+
+
+df['States'] = newind
+
+
+# In[53]:
+
+
+df
+
+
+# In[54]:
+
+
+df.set_index('States')
+
+
+# In[55]:
+
+
+df
+
+
+# In[56]:
+
+
+df.set_index('States',inplace=True)
+
+
+# In[57]:
+
+
+df
+
+
+# In[58]:
+
+
+#The next section is related to multi index and index Hierarchy
+# Index Levels
+outside = ['G1','G1','G1','G2','G2','G2']
+inside = [1,2,3,1,2,3]
+hier_index = list(zip(outside,inside))
+hier_index = pd.MultiIndex.from_tuples(hier_index)
+
+
+# In[59]:
+
+
+hier_index
+
+
+# In[61]:
+
+
+df = pd.DataFrame(np.random.randn(6,2),index=hier_index,columns=['A','B'])
+df
+
+
+# In[65]:
+
+
+df.loc['G1'].loc[1]
+
+
+# 
+
+# In[67]:
+
+
+df.index.names
+
+
+# In[68]:
+
+
+df.index.names = ['Groups','Num']
+
+
+# In[69]:
+
+
+df
+
+
+# In[70]:
+
+
+df.xs("G1")
+
+
+# In[71]:
+
+
+df.xs(['G1',1])
+
+
+# In[72]:
+
+
+df.xs(1,level="Num")
+
+
 # In[ ]:
 
 
